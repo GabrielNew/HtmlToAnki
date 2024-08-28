ebdcf404596d8416be949c634b2b2f09a0477d52
GabrielNew <gabrielfai70@gmail.com>
2024-08-27 22:37:37 -0300

    Add images flashcards

--- AnkiGenerator.py
+++ AnkiGenerator.py
@@ -1,17 +1,18 @@
 import genanki
 from bs4 import BeautifulSoup
 from random import randint
-from dotenv import load_dotenv
 import os
-from time import sleep
+import shutil
+
+IMAGES_PATH = r'C:\Users\Teste\OneDrive\Documents\Programação\Python\AnkiFlashcardGenerator\images'
 
 def ReadFileHTML():
   with open("C:\\Users\\Teste\\OneDrive\\Documents\\Programação\\Python\\AnkiFlashcardGenerator\\HTML_FLASHCARD\\testeFlashcard2.html", "r", encoding="utf-8") as f:
     soup = BeautifulSoup(f, 'html.parser')
   return soup
 
-def CreateDeck():
-  return genanki.Deck(deck_id = 1, name='testDeck2')
+def CreateDeck(deckName):
+  return genanki.Deck(deck_id = randint(1, 99999), name=deckName)
   
 def CreateBasicDeckNote(question, answer):
   return genanki.Note(model=genanki.BASIC_MODEL, fields=[question, answer])
@@ -44,16 +45,49 @@ def CreateClozeDeckNote(questionAndAnswer):
 def AddCardDeck(deckNote):
   myDeck.add_note(deckNote)
 
-def ExportApkgFile():
-  genanki.Package(myDeck).write_to_file('testScript.apkg')
+def ExportApkgFile(deckName):
+  my_package = genanki.Package(myDeck)
+  my_package.media_files = list_files(IMAGES_PATH)
+  my_package.write_to_file(deckName+'.apkg')
 
-#load_dotenv()
-#HTML_PATH = os.getenv('HTML_PATH')
-#print(HTML_PATH)
+def list_files(dir_path):
+    # list to store files
+    res = []
+    try:
+        for file_path in os.listdir(dir_path):
+            if os.path.isfile(os.path.join(dir_path, file_path)):
+                res.append(file_path)
+    except FileNotFoundError:
+        print(f"The directory {dir_path} does not exist")
+    except PermissionError:
+        print(f"Permission denied to access the directory {dir_path}")
+    except OSError as e:
+        print(f"An OS error occurred: {e}")
+    return res
 
-total_questions = 0
-myDeck = CreateDeck()
+def copy_archives(origin, destination):
+    files = os.listdir(origin)
+    
+    copy_files = []
+    for file in files:
+        origin_path = os.path.join(origin, file)
+        destination_path = os.path.join(destination, file)
+        if os.path.isfile(origin_path):
+            shutil.copy(origin_path, destination_path)
+            copy_files.append(destination_path)
+    
+    return copy_files
+
+def delete_files(archives):
+    for file in archives:
+        if os.path.isfile(file):
+            os.remove(file)
+        else:
+            print(f"The file {file} doesn't exist.")
+
 soup = ReadFileHTML()
+deckName = soup.find('header').text
+myDeck = CreateDeck(deckName)
 questionsAndAnswers = soup.find_all('ul', 'toggle')
 
 for toggle in questionsAndAnswers:
@@ -61,38 +95,13 @@ for toggle in questionsAndAnswers:
     questionAndAnswer = FormatClozeDeck(toggle)
     deckNote = CreateClozeDeckNote(questionAndAnswer)
     AddCardDeck(deckNote)
-    total_questions += 1
   else:
-    list_tags = []
-    i = 0
-    for test in toggle.find_all(True):
-      if(i > 1):
-        list_tags.append(test.name)
-      i += 1
-    list_tags = list(dict.fromkeys(list_tags))
-    list_tags.remove('summary')
-    if('strong' in list_tags):
-      list_tags.remove('strong')
-    if('p' in list_tags and (len(list_tags) == 1)):
-      # JUST P TAG
-      question = toggle.find('summary').text
-      answer = ''
-      if(len(toggle.find_all('p')) > 1):
-        answers = toggle.find_all('p')
-        for ans in answers:
-          answer += ans.text + ' '
-      else:
-        answer = toggle.find('p').text
-      deckNote = CreateBasicDeckNote(question, answer)
-      AddCardDeck(deckNote)
-    elif('li' in list_tags and (len(list_tags) == 1)):
-      print('JUST LI TAG')
-    elif('figure' in list_tags and (len(list_tags) == 1)):
-      print('JUST FIGURE TAG')
-    elif('ol' in list_tags and 'li' in list_tags and (len(list_tags) == 2)):
-      print('JUST OL AND LI TAGS')
-    elif('p' in list_tags and 'ul' in list_tags and 'li' in list_tags and (len(list_tags) == 3)):
-      print('JUST P AND UL AND LI TAGS')
-    list_tags.clear()
-    
-ExportApkgFile()
+    question = toggle.find('summary').text
+    toggle.find('summary').replace_with('')
+    answer = str(toggle.find('details')).replace('<details open="">', '').replace('</details>', '')
+    deckNote = CreateBasicDeckNote(question, answer)
+    AddCardDeck(deckNote)
+
+copied_files = copy_archives(r'C:\Users\Teste\OneDrive\Documents\Programação\Python\AnkiFlashcardGenerator\images', r'C:\Users\Teste\OneDrive\Documents\Programação\Python\AnkiFlashcardGenerator')
+ExportApkgFile(deckName.strip())
+delete_files(copied_files)
